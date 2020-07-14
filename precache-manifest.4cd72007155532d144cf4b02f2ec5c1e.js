ab913c162890fb9f2a49426053e72c98b53b39e8
Jair <jair@reservamos.mx>
2020-07-14 15:59:31 -0500

    Updates

---
+++ precache-manifest.4cd72007155532d144cf4b02f2ec5c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b928b82a9371b18f8da9fa1d159662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4da0d9443df2e1689e",
+    "url": "/static/css/main.f5bb3f77.chunk.css"
+  },
+  {
+    "revision": "cd4c19a4a99a80c02acd",
+    "url": "/static/js/2.42f6d231.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f6d231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4da0d9443df2e1689e",
+    "url": "/static/js/main.79ba5dd9.chunk.js"
+  },
+  {
+    "revision": "b2920b87acc9a891b88b",
+    "url": "/static/js/runtime-main.d28f6590.js"
+  }
+]);
